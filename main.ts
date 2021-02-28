4a7fd878115753c07e7814ed403aa75949902771
computerSciencePrestige <37340060+computerSciencePrestige@users.noreply.github.com>
2021-02-28 09:05:27 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,12 @@
 basic.forever(function () {
-    if (input.lightLevel() < 75) {
-        if (input.buttonIsPressed(Button.A)) {
-            basic.showLeds(`
-                # # # # .
-                # # # # .
-                # # # . #
-                # # . . #
-                . . # # #
-                `)
-        }
-        if (input.buttonIsPressed(Button.B)) {
-            basic.showLeds(`
-                . . . . #
-                . . . . #
-                . . . # .
-                . . # # .
-                # # . . .
-                `)
-        }
+    if (input.compassHeading() < 45 || input.compassHeading() == 315) {
+        basic.showLeds(`
+            # . . . #
+            # # . . #
+            # . # . #
+            # . . # #
+            # . . . #
+            `)
     } else {
         basic.showLeds(`
             . . . . .
